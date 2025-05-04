6f6e41341a5e955631a61575951180feb469ec6e
RUN-Projects <82665975+RUN-Projects@users.noreply.github.com>
2025-05-04 12:06:50 -0500

    Match margin and padding to Content page 1

--- src/pages/ConfigureWorkspace/ConfigureWorkspace.jsx
+++ src/pages/ConfigureWorkspace/ConfigureWorkspace.jsx
@@ -45,86 +45,88 @@ function ConfigureWorkspace() {
                   currentId="core-local-workspace"
               />
             </Box>
-            <Box sx={{p: 1, backgroundColor: "#EEE"}} style={{position: 'fixed', top: '48px', bottom: 0, overflow: 'scroll'}}>
-                <Grid2 container spacing={1} sx={{backgroundColor: "#EEE"}}>
-                    <Grid2 item size={11} sx={{backgroundColor: "#FFF"}}>
-                        <Typography variant="h5">
-                            {`${doI18n("pages:core-local-workspace:editing", i18nRef.current, debugRef.current)} ${currentProjectRef.current && currentProjectRef.current.project}`}
-                        </Typography>
-                    </Grid2>
-                    <Grid2 item size={1}  display="flex" justifyContent="flex-end" alignItems="center" sx={{backgroundColor: "#FFF"}}>
-                        <IconButton
-                            onClick={
-                                (e) => {
-                                    console.log(currentProjectRef.current);
-                                    let stateEntries = repos
-                                        .map(r => [r.path, r])
-                                        .filter(re => selectedResources.includes(re[0]) || (currentProjectRef.current && re[0] === Object.values(currentProjectRef.current).join("/")))
-                                        .map(re => (currentProjectRef.current && re[0] === Object.values(currentProjectRef.current).join("/")) ? [re[0], {...re[1], primary: true}]: re)
-                                    navigate(
-                                        "/workspace",
-                                        {
-                                            state: Object.fromEntries(stateEntries)
-                                        }
-                                    );
-                                    e.stopPropagation();
-                                }
-                            }
-                        >
-                            <PlayArrowIcon/>
-                        </IconButton>
-                    </Grid2>
-                    {
-                        repos
-                            .filter(r => r.path !== `_local_/_local_/${currentProjectRef.current && currentProjectRef.current.project}`)
-                            .map(
-                                ((rep, n) => {
-                                        return <>
-                                            <Grid2 key={`${n}-name`} item size={4} sx={{backgroundColor: "#FFF"}}>
-                                                <Stack>
-                                                    <Box><b>{`${rep.name} (${rep.abbreviation})`}</b></Box>
-                                                    {rep.description !== rep.name &&
-                                                        <Box>{rep.description}</Box>
-                                                    }
-                                                </Stack>
-                                            </Grid2>
-                                            <Grid2 key={`${n}-language`} item size={1} sx={{backgroundColor: "#FFF"}}>
-                                                {rep.language_code}
-                                            </Grid2>
-                                            <Grid2 key={`${n}-flavor`} item size={2} sx={{backgroundColor: "#FFF"}}>
-                                                {rep.flavor}
-                                            </Grid2>
-                                            <Grid2 key={`${n}-source`} item size={2} sx={{backgroundColor: "#FFF"}}>
-                                                {
-                                                    rep.path.startsWith("_local_") ?
-                                                        "Local" :
-                                                        rep.path.split("/").slice(0, 2).join(" ")
-                                                }
-                                            </Grid2>
-                                            <Grid2 key={`${n}-date`} item size={2} sx={{backgroundColor: "#FFF"}}>
-                                                {dateFormat(rep.generated_date, "mmm d yyyy")}
-                                            </Grid2>
-                                            <Grid2 key={`${n}-actions`} item size={1} display="flex"
-                                                   justifyContent="flex-end" alignItems="center"
-                                                   sx={{backgroundColor: "#FFF"}}>
-                                                <Checkbox
-                                                    checked={selectedResources.includes(rep.path)}
-                                                    onChange={
-                                                        () => setSelectedResources(
-                                                            selectedResources.includes(rep.path) ?
-                                                                [...selectedResources].filter(s => s !== rep.path) :
-                                                                [...selectedResources, rep.path]
-                                                        )
-                                                    }
-                                                    inputProps={{'aria-label': 'controlled'}}
-                                                />
-                                            </Grid2>
-                                        </>
-                                    }
-                                )
-                            )
-                    }
-                </Grid2>
+            <Box style={{position: 'fixed', top: '48px', bottom: 0, overflow: 'scroll'}}>
+              <Box sx={{backgroundColor: "#EEE", margin: 2}}>
+                  <Grid2 container spacing={1} sx={{backgroundColor: "#EEE"}}>
+                      <Grid2 item size={11} sx={{backgroundColor: "#FFF"}}>
+                          <Typography variant="h5">
+                              {`${doI18n("pages:core-local-workspace:editing", i18nRef.current, debugRef.current)} ${currentProjectRef.current && currentProjectRef.current.project}`}
+                          </Typography>
+                      </Grid2>
+                      <Grid2 item size={1}  display="flex" justifyContent="flex-end" alignItems="center" sx={{backgroundColor: "#FFF"}}>
+                          <IconButton
+                              onClick={
+                                  (e) => {
+                                      console.log(currentProjectRef.current);
+                                      let stateEntries = repos
+                                          .map(r => [r.path, r])
+                                          .filter(re => selectedResources.includes(re[0]) || (currentProjectRef.current && re[0] === Object.values(currentProjectRef.current).join("/")))
+                                          .map(re => (currentProjectRef.current && re[0] === Object.values(currentProjectRef.current).join("/")) ? [re[0], {...re[1], primary: true}]: re)
+                                      navigate(
+                                          "/workspace",
+                                          {
+                                              state: Object.fromEntries(stateEntries)
+                                          }
+                                      );
+                                      e.stopPropagation();
+                                  }
+                              }
+                          >
+                              <PlayArrowIcon/>
+                          </IconButton>
+                      </Grid2>
+                      {
+                          repos
+                              .filter(r => r.path !== `_local_/_local_/${currentProjectRef.current && currentProjectRef.current.project}`)
+                              .map(
+                                  ((rep, n) => {
+                                          return <>
+                                              <Grid2 key={`${n}-name`} item size={4} sx={{backgroundColor: "#FFF"}}>
+                                                  <Stack>
+                                                      <Box><b>{`${rep.name} (${rep.abbreviation})`}</b></Box>
+                                                      {rep.description !== rep.name &&
+                                                          <Box>{rep.description}</Box>
+                                                      }
+                                                  </Stack>
+                                              </Grid2>
+                                              <Grid2 key={`${n}-language`} item size={1} sx={{backgroundColor: "#FFF"}}>
+                                                  {rep.language_code}
+                                              </Grid2>
+                                              <Grid2 key={`${n}-flavor`} item size={2} sx={{backgroundColor: "#FFF"}}>
+                                                  {rep.flavor}
+                                              </Grid2>
+                                              <Grid2 key={`${n}-source`} item size={2} sx={{backgroundColor: "#FFF"}}>
+                                                  {
+                                                      rep.path.startsWith("_local_") ?
+                                                          "Local" :
+                                                          rep.path.split("/").slice(0, 2).join(" ")
+                                                  }
+                                              </Grid2>
+                                              <Grid2 key={`${n}-date`} item size={2} sx={{backgroundColor: "#FFF"}}>
+                                                  {dateFormat(rep.generated_date, "mmm d yyyy")}
+                                              </Grid2>
+                                              <Grid2 key={`${n}-actions`} item size={1} display="flex"
+                                                    justifyContent="flex-end" alignItems="center"
+                                                    sx={{backgroundColor: "#FFF"}}>
+                                                  <Checkbox
+                                                      checked={selectedResources.includes(rep.path)}
+                                                      onChange={
+                                                          () => setSelectedResources(
+                                                              selectedResources.includes(rep.path) ?
+                                                                  [...selectedResources].filter(s => s !== rep.path) :
+                                                                  [...selectedResources, rep.path]
+                                                          )
+                                                      }
+                                                      inputProps={{'aria-label': 'controlled'}}
+                                                  />
+                                              </Grid2>
+                                          </>
+                                      }
+                                  )
+                              )
+                      }
+                  </Grid2>
+              </Box>
             </Box>
         </Box>
 }
