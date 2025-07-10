30348944c7e1b0de5074c91130d2a0eefe6009ab
tidic <tidic84@gmail.com>
2025-07-10 16:07:13 +0200

    added a resource verification for obs images

--- src/munchers/OBS/OBSViewerMuncher.jsx
+++ src/munchers/OBS/OBSViewerMuncher.jsx
@@ -2,6 +2,7 @@ import {useState, useContext, useEffect} from "react";
 import {Box, Stack} from "@mui/material";
 import OBSContext from "../../contexts/obsContext";
 import Markdown from 'react-markdown';
+import RequireResources from "../../components/RequireRessources";
 
 import {
     i18nContext as I18nContext,
@@ -9,9 +10,6 @@ import {
     doI18n,
     getText
 } from "pithekos-lib";
-
-
-
         
 function OBSViewerMuncher({metadata}) {
     const {obs, setObs} = useContext(OBSContext);
@@ -25,11 +23,7 @@ function OBSViewerMuncher({metadata}) {
         let response = await getText(ingredientLink, debugRef.current);
         if (response.ok) {
             let lines = response.text.split("\n\n"); // Récupérer suelement le texte et les images
-            // Version 1 -> Décalé a cause du titre considéré comme premiere phrase du paragraphe
-            // const index = (obs[1] - 1) * 2; // L'index d'un paragraphe
-            // const ingredient = lines[index] ? lines[index] + '\n\n' + lines[index + 1] : lines[index]; 
 
-            // Version 2 -> Titre tout seul
             const index = (obs[1]) * 2 - 1 < 0 ? 0 : (obs[1]) * 2 - 1;  // L'index d'un paragraphe
             const index2 = index === 0 ? undefined : index + 1;
             let ingredient = lines[index];
@@ -40,15 +34,7 @@ function OBSViewerMuncher({metadata}) {
             let imageName = `${obs[0] <= 9 ? `0${obs[0]}` : obs[0]}-${obs[1] <= 9 ? `0${obs[1]}` : obs[1]}`;
             const obsImageLink = `/burrito/ingredient/bytes/git.door43.org/uW/obs_images_360?ipath=360px/obs-en-${imageName}.jpg`
             ingredient = ingredient.replace(/!\[OBS Image\]\([^)]+\)/g, `![OBS Image not found](${obsImageLink})`);
-            
-            // Version 3 -> Le titre fait partie du premier paragraphe donc 3 lignes au premier paragraphe
-            // const index = (obs[1] - 1) * 2 + 1; // L'index d'un paragraphe
-            // let ingredient = "";
-            // if (obs[1] === 1) {
-            //     ingredient = lines[index] ? lines[index] + '\n\n' + lines[index + 1] + '\n\n' + lines[index + 2] : lines[index]; 
-            // } else {
-            //     ingredient = lines[index] ? lines[index] + '\n\n' + lines[index + 1] : lines[index];
-            // }
+
 
             setIngredient(ingredient);
         }
@@ -60,14 +46,29 @@ function OBSViewerMuncher({metadata}) {
         }, [obs]
     );
 
+    const contentSpec = {
+        "general": {
+            "obsimg360": {
+                "_all": {
+                    "dcs": {
+                        "name": "OBS Images 360 (OBSIMG360)",
+                        "repoPath": "git.door43.org/uW/obs_images_360"
+                    }
+                }
+            }
+        }
+    }
+
     return (
-        <Stack sx={{ p:2}}>
-            <div>
-                <Markdown>
-                    {ingredient}
-                </Markdown>
-            </div>
-        </Stack>
+        <RequireResources contentSpec={contentSpec}>
+            <Stack sx={{ p:2}}>
+                <div>
+                    <Markdown>
+                        {ingredient}
+                    </Markdown>
+                </div>
+            </Stack>
+        </RequireResources>
     );
 }
 
