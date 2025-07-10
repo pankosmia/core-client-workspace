c8f776576370e777e8bd947a5b732591e5c9a2c1
eliaspinero <eliasrpt14@gmail.com>
2025-07-10 11:28:52 -0400

    Cleaned the code and removed unnecessary stuff. #43

--- src/munchers/BcvQuestions/BcvQuestionsViewerMuncher.jsx
+++ src/munchers/BcvQuestions/BcvQuestionsViewerMuncher.jsx
@@ -39,15 +39,10 @@ function BcvQuestionsViewerMuncher({metadata}) {
         },
         [systemBcv]
     );
-    
-    const chapterRegex = new RegExp("/\b([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])");
-
-    
 
     const filteredIngredient = ingredient.filter(l => {
 
         const expandedVerseNumbers = (evn) => {
-            console.log("evn", evn);
             const current = evn.split(",");
             const numbersArray = current.map((num) => {
                 if (num.includes("-")){
@@ -61,83 +56,12 @@ function BcvQuestionsViewerMuncher({metadata}) {
               })
             return [...new Set([].concat(...numbersArray))]
           }
-        
-        console.log("expanded",expandedVerseNumbers(l[0].split(":")[1]));
 
         return l[0].split(":")[0] === `${systemBcv.chapterNum}` && expandedVerseNumbers(l[0].split(":")[1]).includes(systemBcv.verseNum)
-
-       /* const expandedVerseNumbers = (arr) => {
-            /* const map1 = arr.map((l) => {
-                if (l[0].split(":")[1].includes("-")){
-                    const startValue = Number(l[0].split(":")[1].split("-")[0]);
-                    const endValue = Number(l[0].split(":")[1].split("-")[1]);
-                    const rangeArray = Array.from({ length: endValue - startValue + 1 }, (_, i) => startValue + i);
-                    return rangeArray;
-                } else {
-                    return [l];
-                }
-            })
-        
-            
-        }; */
-        
-       
-
-       /*  if (l[0].split(":")[0] === `${systemBcv.chapterNum}`) { */
-
-
-
-/*             console.log("chaooooooooooooooooooooooooooooooooooo");
-            if (l[0].split(":")[1].includes(",")) {
-                if (l[0].split(":")[1].split(",")[0] === `${systemBcv.verseNum}`){
-                    return l[0].split(":")[1].split(",")[0] === `${systemBcv.verseNum}`;
-                }
-                if (l[0].split(":")[1].split(",")[1] === `${systemBcv.verseNum}`){
-                    return l[0].split(":")[1].split(",")[1] === `${systemBcv.verseNum}`;
-                }
-            } */
-            /* if (l[0].split(":")[1].includes("-"))  */
-/*                 const startValue = l[0].split(":")[1].includes("-") ? Number(l[0].split(":")[1].split("-")[0]) : null;
-                const endValue = l[0].split(":")[1].includes("-") ? Number(l[0].split(":")[1].split("-")[1]) : null;
-                const rangeArray = Array.from({ length: endValue - startValue + 1 }, (_, i) => startValue + i);
-                console.log(rangeArray);
-
-                if (rangeArray.includes(systemBcv.verseNum)){
-                    return l[0].split(":")[1].split("-")[0] === `${rangeArray[0]}`;
-                } */
-
-                /* const search = rangeArray.find((l) => l === systemBcv.verseNum); */
-                
-                
-
-                /* rangeArray.forEach((r, n) => {
-                    return l[0].split(":")[1].split("-").includes(`${r}`);
-                }) */
-                
-                /* /* for (let i = 0; i < rangeArray.length; i++){
-                    return l[0] === `${systemBcv.chapterNum}:${rangeArray[i]}`
-                } */
-                /* if (l[0].split(":")[1].split("-")[0] === `${systemBcv.verseNum}`){
-                    return l[0].split(":")[1].split("-")[0] === `${systemBcv.verseNum}`;
-                }
-                if (l[0].split(":")[1].split("-")[1] === `${systemBcv.verseNum}`){
-                    return l[0].split(":")[1].split("-")[1] === `${systemBcv.verseNum}`;
-                } */
-            /* }  */
-        /* } */
     });
 
-    const filteredIngredient2 = ingredient.filter(l => {
-        return l[0] === `${systemBcv.chapterNum}:${systemBcv.verseNum}`;
-    });
-
-    const newArray = [...new Set([...filteredIngredient2,...filteredIngredient])];
-
-    const verseQuestions = newArray.map(l => l[5]);
-    const verseAnswers = newArray.map(l => l[6]);
-    console.log(ingredient);
-    /* console.log(filteredIngredient);
-    console.log(filteredIngredient); */
+    const verseQuestions = filteredIngredient.map(l => l[5]);
+    const verseAnswers = filteredIngredient.map(l => l[6]);
 
     return (
         <Box sx={{ flexGrow: 1 }}>
