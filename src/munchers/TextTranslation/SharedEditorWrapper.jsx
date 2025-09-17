202dfdd796e28680fbedcbd4ffe418725ae8c3f4
Mark Howe <mvahowe@gmail.com>
2025-09-17 13:48:12 +0200

    Use Proskomma for USFM <=> USJ

--- src/munchers/TextTranslation/SharedEditorWrapper.jsx
+++ src/munchers/TextTranslation/SharedEditorWrapper.jsx
@@ -6,7 +6,7 @@ import {
     getJson,
     postJson,
     i18nContext as I18nContext,
-    doI18n
+    doI18n, getText
 } from "pithekos-lib";
 import md5sum from 'md5';
 import { enqueueSnackbar } from "notistack";
@@ -15,6 +15,7 @@ import SharedEditor from "./SharedEditor";
 import { useAppReferenceHandler } from "./useAppReferenceHandler";
 import { Box, Typography } from "@mui/material";
 import md5 from "md5";
+import {Proskomma} from "proskomma-core";
 
 function SharedEditorWrapper({ metadata, modified, setModified }) {
     const { bcvRef } = useContext(BcvContext);
@@ -28,23 +29,35 @@ function SharedEditorWrapper({ metadata, modified, setModified }) {
         "TIT"
     )
 
-    // Fetch new USFM as USJ, put in incoming
+    // Fetch new USFM, convert to USJ, put in incoming
     useEffect(
         () => {
             if (!usj || bookCode !== bcvRef.current.bookCode) { // Changes arrive via context after POSTing new BCV from BcvPicker
-                const usfmLink = `/burrito/ingredient/as-usj/${metadata.local_path}?ipath=${bcvRef.current.bookCode}.usfm`;
-                getJson(usfmLink, debugRef.current)
+                const usfmLink = `/burrito/ingredient/raw/${metadata.local_path}?ipath=${bcvRef.current.bookCode}.usfm`;
+                getText(usfmLink, debugRef.current)
                     .then(
                         res => {
                             if (res.ok) {
-                                setUsj(res.json);
-                                setBookCode(bcvRef.current.bookCode);
+                                const usfm = res.text;
+                                try {
+                                    const pk = new Proskomma();
+                                    pk.importDocument({"lang": "eng", "abbr": "xyz"}, "usfm", usfm);
+                                    const query = "{documents {usj}}";
+                                    const result = pk.gqlQuerySync(query);
+                                    const usj = JSON.parse(result.data.documents[0].usj);
+                                    setUsj(usj);
+                                    setBookCode(bcvRef.current.bookCode);
+                                } catch (err) {
+                                    enqueueSnackbar(
+                                        `${doI18n("pages:core-local-workspace:parse_error", i18nRef.current)}: ${res.status}`,
+                                        { variant: "error" }
+                                    );
+                                }
                             } else {
                                 enqueueSnackbar(
                                     `${doI18n("pages:core-local-workspace:load_error", i18nRef.current)}: ${res.status}`,
                                     { variant: "error" }
                                 );
-
                             }
                         }).catch(err => console.log("TextTranslation fetch error", err));
             }
@@ -55,13 +68,24 @@ function SharedEditorWrapper({ metadata, modified, setModified }) {
     const uploadJsonIngredient = async (repoPath, ingredientPath, jsonData, debugBool) => {
         // Convert JSON object to a file
         const jsonString = JSON.stringify(jsonData, null, 2);
+        let usfm;
+        try {
+            const pk = new Proskomma();
+            pk.importDocument({"lang": "eng", "abbr": "xyz"}, "usj", jsonString);
+            const query = "{documents {usfm}}";
+            const result = pk.gqlQuerySync(query);
+            usfm = JSON.parse(result.data.documents[0].usfm);
+        } catch (err) {
+            enqueueSnackbar(
+                `${doI18n("pages:core-local-workspace:usfm_conversion_error", i18nRef.current)}: ${err}`,
+                { variant: "error" }
+            );
+        }
         const response = await postJson(
-            `/burrito/ingredient/as-usj/${repoPath}?ipath=${ingredientPath}`,
-            jsonString,
+            `/burrito/ingredient/raw/${repoPath}?ipath=${ingredientPath}`,
+            usfm,
             debugBool
         );
-        console.log("response", response)
-
         if (response.ok) {
             enqueueSnackbar(
                 `${doI18n("pages:core-local-workspace:saved", i18nRef.current)}`,
@@ -94,7 +118,7 @@ function SharedEditorWrapper({ metadata, modified, setModified }) {
          */
         const recoverableState = editorState.toJSON();
         const newStateChecksum = md5(JSON.stringify(recoverableState,null, 2));
-        console.log(savedChecksum, newStateChecksum);
+        debugRef && debugRef.current && console.log(savedChecksum, newStateChecksum);
         const notSame = newStateChecksum !== savedChecksum;
         if (notSame) {
             setSavedChecksum(newStateChecksum);
@@ -107,31 +131,6 @@ function SharedEditorWrapper({ metadata, modified, setModified }) {
         debugRef && debugRef.current && console.log("onHistoryChange", recoverableState);
     }
 
-    // Récupération du code Electron 
-    // const isModified = () => {
-    //     const chapterIndex = obs[0];
-    //     const originalChecksum = chapterChecksums[chapterIndex];
-    //     if (!originalChecksum) {
-    //         return false;
-    //     }
-    //     const currentChecksum = calculateChapterChecksum(ingredient[chapterIndex]);
-
-    //     return originalChecksum !== currentChecksum;
-    // };
-
-    // // Intercepter les tentatives de navigation
-    // useEffect(() => {
-      //     const isElectron = !!window.electronAPI;
-    //     if (isElectron) {
-    //         if (isModified()) {
-    //             window.electronAPI.setCanClose(false);
-    //         } else {
-    //             window.electronAPI.setCanClose(true);
-    //         }
-    //     }
-    // }, [isModified]);
-
-  
     const { referenceHandler } = useAppReferenceHandler();
 
     return (
