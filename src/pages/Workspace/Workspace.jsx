c78bb20ade5f525a04ec52aadba51abbcd912d0e
lmeline <loise13@hotmail.fr>
2025-10-02 10:37:24 +0200

    fix merge conflits

--- src/pages/Workspace/Workspace.jsx
+++ src/pages/Workspace/Workspace.jsx
@@ -23,6 +23,7 @@ const paneStyle = {
 
 
 const Workspace = () => {
+    const { i18nRef } = useContext(i18nContext);
     const { typographyRef } = useContext(typographyContext);
     const locationState = Object.entries(useLocation().state);
     const resources = locationState
@@ -67,7 +68,7 @@ const Workspace = () => {
     const DistractionToggle = ({ distractionModeCount, setDistractionModeCount, resource }) => {
         const { i18nRef } = useContext(i18nContext);
         return (
-            <Stack sx={{marginLeft: "60rem"}}  >
+            <Stack sx={{ marginLeft: "60rem" }}  >
                 <Chip
                     onClick={() => { setDistractionModeCount(distractionModeCount + 1); }}
                     icon={<CenterFocusWeakIcon color={(distractionModeCount % 2) === 0 ? "#FFF" : "#555"} />}
