0ce7835dc0b883f20a8756782e4ec1678f48de66
eliaspinero <eliasrpt14@gmail.com>
2025-10-30 01:25:33 -0400

    Cleaned the code

--- src/munchers/BcvVideos/BcvVideosViewerMuncher.jsx
+++ src/munchers/BcvVideos/BcvVideosViewerMuncher.jsx
@@ -89,7 +89,7 @@ function BcvImagesViewerMuncher({metadata}) {
 
     useEffect(
         () => {
-            // clear captions when verse notes change to avoid stale text
+            // Clear captions when verse notes change to avoid stale text
             setVerseCaptions([]);
             if (verseNotes.length > 0){
             const doVerseCaptions = async () => {
@@ -138,14 +138,11 @@ function BcvImagesViewerMuncher({metadata}) {
             );
         }
     };
-    console.log(ingredient);
-    console.log(verseNotes);
     
     return (
         <Box className="h-full w-full flex flex-col overflow-hidden">
             <div className="flex flex-col items-center justify-center p-2">
                 <h5>{`${metadata.name} (${systemBcv.bookCode} ${systemBcv.chapterNum}:${systemBcv.verseNum})`}</h5>
-                {/* <h6>{doI18n("munchers:bcv_notes_viewer:title", i18nRef.current)}</h6> */}
             </div>
             <div className="flex-1 min-h-0 min-w-0 overflow-hidden p-2">
                 {ingredient &&
