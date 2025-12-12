7408406b7fd41f361d029804355c92e3ca90cce1
Mark Howe <mvahowe@gmail.com>
2025-12-12 17:57:15 +0100

    Catch no-unit-in-block issue

--- src/munchers/TextTranslation/SimplifiedEditor/components/EditableBibleBlock.jsx
+++ src/munchers/TextTranslation/SimplifiedEditor/components/EditableBibleBlock.jsx
@@ -7,7 +7,7 @@ export default function EditableBibleBlock({scriptureJson, setScriptureJson, pos
         <div style={{ flexDirection: "column",textAlign:"left" }} className={scriptureJson.blocks[position[0]].tag}>
             {/* <EditableTag filterScriptureJsonChapter={filterScriptureJsonChapter} block={block} position={[...position]}/> */}
             <span className={scriptureJson.blocks[position[0]].tag} >
-                {scriptureJson.blocks[position[0]].units.map((u, i) => (
+                {scriptureJson.blocks[position[0]].units && scriptureJson.blocks[position[0]].units.map((u, i) => (
                     <span>
                         <span className="marks_verses_label">{u.verses}</span>
                         <EditableSpan scriptureJson={scriptureJson} setScriptureJson={setScriptureJson} position={[...position, i]}/>
