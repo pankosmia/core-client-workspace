dc52c985052b88eccb930a3f1f4edade814ea268
lmeline <loise13@hotmail.fr>
2025-12-15 17:26:46 +0100

    fonction recursive pour afficher les tags

--- src/munchers/TextTranslation/SimplifiedEditor/components/EditableTag.jsx
+++ src/munchers/TextTranslation/SimplifiedEditor/components/EditableTag.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Menu, MenuItem } from "@mui/material";
 import { updateBlockTag } from '../Controller';
+import mainBlockMenu from '../menus/main_blocks.json';
+
 export default function EditableTag({ scriptureJson, setScriptureJson, position }) {
     const [anchorEl, setAnchorEl] = useState(null);
     const incomingBlock = scriptureJson.blocks[position[0]];
@@ -19,11 +21,44 @@ export default function EditableTag({ scriptureJson, setScriptureJson, position 
     const handleClose = () => {
         setAnchorEl(null);
     };
+    console.log("jsonMain", mainBlockMenu);
+
+    function doMenu(menuSpec) {
+        return (
+            menuSpec.map((t, n) => {
+                if (t[0] === 'submenu') {
+                    console.log("map 1")
+                    return (
+                        <Menu
+                            key={n}
+                            open={true}
+                        >
+                            {doMenu(t[2])}
+                        </Menu>
+                    )
+                }
+                else {
+                    console.log("map 2")
+                    return (
+                        <MenuItem
+                            key={n}
+                            onClick={() => { changeValue(t); handleClose() }}>
+                            {t[1]}
+                        </MenuItem>
+                    )
+                }
+
+            })
+        )
+
+    }
+    //console.log(doMenu())
+
 
     return (
         <>
             <span
-                style={{fontFamily: "monospace", fontSize: "medium", paddingRight: "1em" }}
+                style={{ fontFamily: "monospace", fontSize: "medium", paddingRight: "1em" }}
                 onClick={(event) => { setAnchorEl(event.currentTarget) }}
             >
                 {value}
@@ -36,13 +71,8 @@ export default function EditableTag({ scriptureJson, setScriptureJson, position 
                 display="inline"
             >
                 {
-                    ["p", "m", "li", "li2", "li3"].map((t, n) =>
-                        <MenuItem
-                            key={n}
-                            onClick={() => { changeValue(t); handleClose() }}>
-                            {t}
-                        </MenuItem>
-                    )
+
+                    doMenu(mainBlockMenu)
                 }
 
             </Menu>
