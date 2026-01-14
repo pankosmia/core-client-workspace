c747b02f7c015becbb2265f6d213d9ce93298969
RUN-Projects <82665975+RUN-Projects@users.noreply.github.com>
2026-01-14 15:22:41 -0600

    Rm unused files per knip

--- src/munchers/TextTranslation/SimplifiedEditor/components/EditableChapter.jsx
+++
@@ -1,9 +0,0 @@
-export default function EditableChapter({ block }) {
-
-    return (
-        <div className="marks_chapter_label" style={{float: "none"}}>
-            <span>{block.chapter}</span>
-        </div>
-    );
-
-}
