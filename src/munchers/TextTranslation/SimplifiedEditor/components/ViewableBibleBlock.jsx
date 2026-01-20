91fc9b89bf64061d94ea72260abc52c76086a1a4
eliaspinero <eliasrpt14@gmail.com>
2026-01-20 09:48:47 -0400

    Scroll to verse and some padding.

--- src/munchers/TextTranslation/SimplifiedEditor/components/ViewableBibleBlock.jsx
+++ src/munchers/TextTranslation/SimplifiedEditor/components/ViewableBibleBlock.jsx
@@ -1,18 +1,39 @@
-import {useContext, useEffect} from "react";
+import {useContext, useEffect, useRef} from "react";
 import {bcvContext} from "pithekos-lib";
 
-export default function ViewableBibleBlock({blockJson }) {
+export default function ViewableBibleBlock({ blockJson }) {
+    const { systemBcv } = useContext(bcvContext);
 
-    const {systemBcv} = useContext(bcvContext);
+    const versesRefs = useRef({});
 
-    return <div className={blockJson.tag}>
-        {blockJson?.units?.map((u, i) => {
-            console.log("system", systemBcv);
-            console.log("u.verses", u.verses);
-            return <span style={(systemBcv.verseNum === Number(u.verses)) ? { backgroundColor: "#CCC" } : {}}>
-                <span className="marks_verses_label">{u.verses}</span>
-                <span style={{ paddingRight: "2pt" }} >{u.content[0]}</span>
-            </span>
-        })}
-    </div>;
+    useEffect(() => {
+        const verseToScroll = systemBcv.verseNum;  
+        if (verseToScroll && versesRefs.current[verseToScroll]) {
+            versesRefs.current[verseToScroll].scrollIntoView({
+                behavior: "auto",
+                block: "center",
+            });
+        }
+    }, [systemBcv.verseNum]); 
+
+    return (
+        <div 
+            className={blockJson.tag} 
+            style={{ padding: "2px 12px",  width: "100%", boxSizing: "border-box" }}
+        >
+            {blockJson?.units?.map((u, i) => {
+                const isSelected = Number(systemBcv.verseNum) === Number(u.verses);
+                return (
+                    <span 
+                        key={`${u.verses}-${i}`}
+                        ref={(el) => (versesRefs.current[u.verses] = el)}
+                        style={isSelected ? { backgroundColor: "#CCC" } : {}}
+                    >
+                        <span className="marks_verses_label">{u.verses}</span>
+                        <span style={{ paddingRight: "2pt" }}>{u.content[0]}</span>
+                    </span>
+                );
+            })}
+        </div>
+    );
 }
