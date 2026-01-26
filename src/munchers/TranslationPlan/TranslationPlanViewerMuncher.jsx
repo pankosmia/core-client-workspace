5dbfb5a871d4e436bc1d9028520d24198ae7665a
Mark Howe <mvahowe@gmail.com>
2026-01-26 13:34:15 +0100

    Scripture renderer to new file

--- src/munchers/TranslationPlan/TranslationPlanViewerMuncher.jsx
+++ src/munchers/TranslationPlan/TranslationPlanViewerMuncher.jsx
@@ -20,6 +20,7 @@ import ExtractJsonValues from "../helpers/ExtractJsonValues";
 import InformationDialog from "./InformationDialog";
 import processUsfm from "./processUsfm";
 import ScripturePicker from "./ScripturePicker";
+import ScriptureField from './ScriptureField';
 
 function TranslationPlanViewerMuncher({metadata}) {
     const [planIngredient, setPlanIngredient] = useState();
@@ -375,52 +376,12 @@ function TranslationPlanViewerMuncher({metadata}) {
                                             );
                                         }
                                         if (field.type === "scripture") {
-                                            if (Object.keys(verseText).length > 0) {
-                                                let chapterN = "0"
-                                                return (
-                                                    <Box dir={selectedBurritoTextDir} key={i}>
-                                                        {
-                                                            section.paragraphs
-                                                                .map(
-                                                                    p => {
-                                                                        if (p.units) {
-                                                                            const c = p.units[0].split(":")[0]
-                                                                            const newChapter = c !== chapterN
-                                                                            if (newChapter) {
-                                                                                chapterN = c
-                                                                            }
-                                                                            return (
-                                                                                <>
-                                                                                    {newChapter && <div
-                                                                                        className="marks_chapter_label">{c}</div>}
-                                                                                    <div className={p.paraTag}>
-                                                                                        {
-                                                                                            p.units.map(
-                                                                                                cv => <span>
-                                                                                            <span
-                                                                                                className="marks_verses_label">{cv.split(":")[1]} </span>
-                                                                                            <span>{verseText[cv.split(":")[0]][cv.split(":")[1]]} </span>
-                                                                                        </span>
-                                                                                            )
-                                                                                        }
-                                                                                    </div>
-                                                                                </>
-                                                                            )
-                                                                        } else {
-                                                                            return <div className={p.paraTag}>
-                                                                                {section.fieldInitialValues[p.name]}
-                                                                                {" "}
-                                                                                ({p.cv.join(" - ")})
-                                                                            </div>
-                                                                        }
-                                                                    }
-                                                                )
-                                                        }
-                                                    </Box>
-                                                );
-                                            } else {
-                                                return <Typography><b><i>{doI18n("pages:core-local-workspace:no_scripture_for_story", i18nRef.current)}</i></b></Typography>
-                                            }
+                                            return <ScriptureField
+                                                key={i}
+                                                section={section}
+                                                verseText={verseText}
+                                                selectedBurritoTextDir={selectedBurritoTextDir}
+                                            />
                                         } else {
                                             return <Typography> loading ...</Typography>
                                         }
