dd1c0524cab3d11e026f1dfd27b82429adb23ba5
lmeline <loise13@hotmail.fr>
2026-02-26 13:07:08 +0100

    change dialog information

--- src/crunchers/Rhakos/InformationDialog.jsx
+++ src/crunchers/Rhakos/InformationDialog.jsx
@@ -14,31 +14,34 @@ export default function InformationDialogRhakos({ open, close, response }) {
             closeFn={() => close(false)}
         >
             <DialogContent>
-                    <>
-                        <DialogContentText>
-                            {doI18n("pages:core-local-workspace:model_rhakos", i18nRef.current)} :  {response?.json?.model_name}
-                        </DialogContentText>
-                        <DialogContentText>
-                            {doI18n("pages:core-local-workspace:bcv_rhakos", i18nRef.current)} :  {response?.json?.book}   {response?.json?.from_chapter}   {response?.json?.from_verse}
-                        </DialogContentText>
-                        <DialogContentText>
-                            {response?.json?.quantized === true ?
-                                `${doI18n("pages:core-local-workspace:quantized_rhakos", i18nRef.current)} :  true`
-                                :`${doI18n("pages:core-local-workspace:quantized_rhakos", i18nRef.current)} :  false`}
-                        </DialogContentText>
-                        <DialogContentText>
-                            {doI18n("pages:core-local-workspace:topk_rhakos", i18nRef.current)} : {response?.json?.top_k}
-                        </DialogContentText>
-                        <DialogContentText>
-                            {doI18n("pages:core-local-workspace:elapsed_rhakos", i18nRef.current)} : {response?.json?.elapsed}
-                        </DialogContentText>
-                        <DialogContentText>
-                            {doI18n("pages:core-local-workspace:temperature_rhakos", i18nRef.current)} :  {response?.json?.temperature}
-                        </DialogContentText>
-                        <DialogContentText>
-                            {doI18n("pages:core-local-workspace:submitted_rhakos", i18nRef.current)} :  {response?.json?.submitted}
-                        </DialogContentText>
-                    </>
+                <>
+                    <DialogContentText>
+                        {doI18n("pages:core-local-workspace:elapsed_rhakos", i18nRef.current)} : {parseFloat(response?.json?.elapsed.toFixed(2))}  {doI18n("pages:core-local-workspace:second", i18nRef.current)}
+                    </DialogContentText>
+                    <DialogContentText>
+                        {doI18n("pages:core-local-workspace:submitted_rhakos", i18nRef.current)} :
+                        {response?.json?.submitted
+                            ? new Date(response.json.submitted * 1000).toLocaleString()
+                            : "—"}
+                    </DialogContentText>
+                    <DialogContentText>
+                        {doI18n("pages:core-local-workspace:model_rhakos", i18nRef.current)} :  {response?.json?.model_name}
+                    </DialogContentText>
+                    <DialogContentText>
+                        {doI18n("pages:core-local-workspace:bcv_rhakos", i18nRef.current)} :  {response?.json?.book}   {response?.json?.from_chapter}   {response?.json?.from_verse}
+                    </DialogContentText>
+                    <DialogContentText>
+                        {response?.json?.quantized === true ?
+                            `${doI18n("pages:core-local-workspace:quantized_rhakos", i18nRef.current)} :  true`
+                            : `${doI18n("pages:core-local-workspace:quantized_rhakos", i18nRef.current)} :  false`}
+                    </DialogContentText>
+                    <DialogContentText>
+                        {doI18n("pages:core-local-workspace:topk_rhakos", i18nRef.current)} : {response?.json?.top_k}
+                    </DialogContentText>
+                    <DialogContentText>
+                        {doI18n("pages:core-local-workspace:temperature_rhakos", i18nRef.current)} :  {response?.json?.temperature}
+                    </DialogContentText>
+                </>
             </DialogContent>
         </PanDialog>
     )
