3e6f86e69554353c9c5dd12bd368dcebadd0e6ee
Mark Howe <mvahowe@gmail.com>
2026-03-04 13:56:17 +0100

    Find pertinent resources

--- src/crunchers/Rhakos/DialogResources.jsx
+++ src/crunchers/Rhakos/DialogResources.jsx
@@ -1,22 +1,62 @@
-import {
-  DialogContent,
-  Grid2,
-  Typography,
-} from "@mui/material";
-import LlmModelPicker from "./LlmModelPicker";
-import NumberPicker from "./NumberPicker";
+import { DialogContent, Grid2, Typography } from "@mui/material";
 import { PanDialog } from "pankosmia-rcl";
-import { doI18n } from "pithekos-lib";
-import { useContext } from "react";
-import { i18nContext } from "pankosmia-rcl";
+import { doI18n, getJson, getAndSetJson } from "pithekos-lib";
+import { useContext, useEffect, useState } from "react";
+import { i18nContext, debugContext, bcvContext } from "pankosmia-rcl";
 
 export default function DialogResources({
   open,
   close,
   resources,
-  setResources
+  setResources,
 }) {
   const { i18nRef } = useContext(i18nContext);
+  const { debugRef } = useContext(debugContext);
+  const { systemBcv } = useContext(bcvContext);
+
+  const [summaries, setSummaries] = useState({});
+  const [language, setLanguage] = useState("en");
+
+  useEffect(() => {
+    const getResourceOptions = async () => {
+      getAndSetJson({
+        url: "/burrito/metadata/summaries",
+        setter: setSummaries,
+        debug: debugRef.current,
+      });
+    };
+    getResourceOptions().then();
+  }, [debugRef]);
+
+  useEffect(() => {
+    const getLanguage = async () => {
+      const gotLanguages = await getJson(
+        "/settings/languages",
+        debugRef.current,
+      );
+      setLanguage(gotLanguages.json[0]);
+    };
+    getLanguage().then();
+  }, [debugRef, open]);
+
+  useEffect(() => {
+    const filteredRepos = Object.entries(summaries)
+      .filter(
+        ra => {
+          return ra[1].language_code === language &&
+          ra[1].book_codes.includes(systemBcv.bookCode)
+  })
+      .map((r) => {
+        return { ...r[1], path: r[0] };
+      });
+    setResources({
+      translations: filteredRepos.filter(
+        (ro) => ro.flavor === "textTranslation",
+      ),
+      juxta: filteredRepos.filter((ro) => ro.flavor === "x-juxtalinear"),
+      notes: filteredRepos.filter((ro) => ro.flavor === "x-bcvnotes"),
+    });
+  }, [summaries, setResources, systemBcv, language]);
 
   return (
     <PanDialog
